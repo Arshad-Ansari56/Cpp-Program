6cc68bbd5f21cd231b27a5852ac26d3c8dc9c3ff
Arshad-Ansari56 <arshad4043ans@gmail.com>
2025-10-17 12:03:14 +0530

    Sum

---
+++ Sum.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int a,b,sum;
+    cout<<"enter a number:";
+   cin>>a;
+    cout<<"enter a number:";
+   cin>>b;
+   sum =a+b;
+   cout<<"sum="<<sum<<endl;
+    return 0;
+}
